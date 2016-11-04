36d019c030bc9959516849826f5b23e4144781a9
Douwe Koopmans <douwekoopmans@hotmail.com>
2016-11-04 20:18:04 +0100

    add unit tests to format class

---
+++ src/test/java/com/fallingdutchman/youtuberedditbot/formatting/FormatterTest.java
@@ -0,0 +1,49 @@
+package com.fallingdutchman.youtuberedditbot.formatting;
+
+import com.google.common.collect.Maps;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+
+/**
+ * Created by douwe on 4-11-16.
+ */
+@RunWith(Parameterized.class)
+public class FormatterTest {
+
+    @Parameterized.Parameter(0) public String format;
+    @Parameterized.Parameter(1) public String input;
+    @Parameterized.Parameter(2) public String expected;
+    private Formatter formatter;
+
+    @Before
+    public void setup() {
+         formatter = new Formatter(format);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                        {"${test}", "foo bar\n", "foo bar\n  "},
+                        {"***\n${test}\n***", "foo bar", "***\n  foo bar\n  ***"},
+                        {"***\n\n***", "foo bar", "***\n  \n  ***"},
+                        {"***foo***", "bar", "***foo***"}
+                }
+        );
+    }
+
+    @Test
+    public void format() throws Exception {
+        final HashMap<String, String> values = Maps.newHashMap();
+        values.put("test", input);
+
+        Assert.assertEquals(expected, formatter.format(values));
+    }
+
+}
